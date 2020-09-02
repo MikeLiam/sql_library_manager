d72f08a6a319bc97a6d29825aa2e3a136e5c56b2
MikeLiam <miguelalcalaromero@gmail.com>
2020-09-03 00:10:36 +0200

    selected pagination

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
+const { Op } = require("sequelize");
 // Error helper to create errors
 const helper = require('../errorHandlers');
 
@@ -16,11 +17,31 @@ function asyncHandler(cb){
   }
 }
 
+function prepareForPagination(books,currentPage) {
+  const limit = books.length;
+  const booksPerPage = 5;
+  const pageCount = Math.ceil(limit / booksPerPage)
+  const pageLinks = Array.from(Array(pageCount), (_, i) => i + 1);
+  const start = (currentPage -1) * booksPerPage
+  const end = (currentPage * booksPerPage) > limit ? limit : (currentPage * booksPerPage)
+  const currentBooks = [...books.slice(start, end)]
+
+  return {currentBooks , pageLinks};
+}
+
 /* GET books listing. */
 router.get('/', asyncHandler(async (req, res) => {
-  const books = await Book.findAll({ order: [[ "title", "ASC" ]] });
-  res.render("books/index", { books, title: "Books" });
+  res.redirect(`books/page/1`)
 }));
+
+router.get('/page/:pagenumber', asyncHandler(async (req, res) => {
+  const books = await Book.findAll({ order: [[ "title", "ASC" ]] });
+  const currentPage = req.params.pagenumber;
+  const {currentBooks, pageLinks} = prepareForPagination(books, currentPage)
+
+  res.render("books/index", { books: currentBooks, title: "Books", pageLinks, currentPage, path: "/books/page/" });
+}));
+
 
 /* Create a new book form. */
 router.get('/new', (req, res) => {
@@ -42,6 +63,56 @@ router.post('/new', asyncHandler(async (req, res) => {
     }  
   }
 
+}));
+
+/* Search book. */
+router.get("/search/:query?", asyncHandler(async (req, res, next) => {
+  const query = req.query.search;
+  if (query === "") {
+    res.redirect('/')
+  }
+  try {
+    const books = await Book.findAll({
+      where: {
+        [Op.or]: [
+          {title: {[Op.like]: `%${query}%`}},
+          {author: {[Op.like]: `%${query}%`}},
+          {genre: {[Op.like]: `%${query}%`}},
+          {year: {[Op.like]: `%${query}%`}}
+        ]
+      }
+    });
+
+    if(books.length !== 0) {
+      res.redirect(`/books/search/${query}/page/1`)
+    } else {
+      // else create and pass an error with friendly message handleError
+      next(helper.createErrorHelper(418, 
+        ` book not found`));
+    }
+  } catch (error) {
+    throw error; // error caught in the asyncHandler's catch block
+  }
+
+}));
+
+router.get('/search/:query/page/:pagenumber', asyncHandler(async (req, res) => {
+  const query = req.params.query;
+  console.log(query)
+  const books = await Book.findAll({
+    where: {
+      [Op.or]: [
+        {title: {[Op.like]: `%${query}%`}},
+        {author: {[Op.like]: `%${query}%`}},
+        {genre: {[Op.like]: `%${query}%`}},
+        {year: {[Op.like]: `%${query}%`}}
+      ]
+    }
+  });
+  const currentPage = req.params.pagenumber;
+  const {currentBooks, pageLinks} = prepareForPagination(books, currentPage)
+
+  res.render("books/index", { books: currentBooks, title: "Books", pageLinks, currentPage, path: `/books/search/${query}/page/` });
 }));
 
 /* GET individual book. */
